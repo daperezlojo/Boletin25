99ac8b0d51fd688ce93863e8add3713b351f69be
daperezlojo <32535705+daperezlojo@users.noreply.github.com>
2018-06-03 20:14:39 +0200

    .

---
+++ src/boletin25/Boletin25.java
@@ -0,0 +1,16 @@
+
+package boletin25;
+
+
+public class Boletin25 {
+
+  
+    public static void main(String[] args) {
+       
+        Bonoloto obx = new Bonoloto();
+        
+        obx.setVisible(true);
+        
+    }
+    
+}
